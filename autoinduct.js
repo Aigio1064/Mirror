b66fed42f749b08f4f2a321f7d77e694aa7835e0
Aigio1064 <97342833+Aigio1064@users.noreply.github.com>
2022-10-30 07:43:34 +0800

    Initial commit

--- autoinduct.js
+++ autoinduct.js
@@ -22,8 +22,37 @@
             "https://unpkg.com/react@18.2.0/cjs/react.production.min.js" // 来自https://unpkg.com/
         ]
     };
-    // 元素创建器 ↓ 这条代码遵循 Apache 协议，原作者还是我，源在另一个库里
-    function createNewElement(Parent,Element,Content,Attributes,Styles){let $Element=document.createElement(Element);if(!!Content){$Element.appendChild(document.createTextNode(Content));};if(!!Attributes){Attributes.forEach($a=>{if(RegExp("=").test($a)){let $b=$a.split("=");$Element.setAttribute($b[0],$b[1]);}else{$Element.setAttribute($a);}});};if(!!Styles){Styles.forEach($a=>{if(RegExp("=").test($a)){let $b=$a.split("=");$Element.setAttribute($b[0],$b[1]);}else{$Element.setAttribute($a);}});};document.querySelector(Parent).appendChild($Element);};
+    // 元素创建器 ↓ 此代码遵循 Apache 协议，原作者还是我，源在另一个库里
+    function createNewElement(Parent, Element, Content, Attributes, Styles) {
+        let $Element = document.createElement(Element);
+        if (!!Content) {
+            $Element.appendChild(document.createTextNode(Content));
+        };
+        if (!!Attributes) {
+            Attributes.forEach($a => {
+                if (RegExp("=").test($a)) {
+                    let $b = $a.split("=");
+                    $Element.setAttribute($b[0], $b[1]);
+                } else {
+                    let $b = document.createAttribute($a);
+                    $Element.setAttributeNode($b)
+                }
+            });
+        };
+        if (!!Styles) {
+            Styles.forEach($a => {
+                if (RegExp("=").test($a)) {
+                    let $b = $a.split("=");
+                    $Element.setAttribute($b[0], $b[1]);
+                } else {
+                    let $b = document.createAttribute($a);
+                    $Element.setAttributeNode($b)
+                }
+            });
+        };
+        document.querySelector(Parent).appendChild($Element);
+    };
+    // ↑ Apache license END
     // 检测需要的资源
     let $el = document.querySelector("[a-srclist]").getAttribute("a-srclist");
     if (RegExp("jquery","i").test($el)) {
@@ -38,7 +67,7 @@
     };
     if (RegExp("react","i").test($el)) {
         $resourcesList.react.forEach($a => {
-            createNewElement("head","script","",["src="+$a,"crossorigin"])
+            createNewElement("head","script","",["src="+$a,"crossorigin=\'\'"])
         })
     };
     // 释放变量
