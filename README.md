fdac131a9c741e2d412147f05d9cb09a5e429d63
Aigio1064 <97342833+Aigio1064@users.noreply.github.com>
2022-10-30 06:50:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# Mirror  
+**此储存库因个人需要而存在，存放于 `GitHub` 储存库中。**  
+**此储存库遵循 [MIT](./LICENSE) 协议 !!!**  
+**内部文件遵循文件作者使用的协议，不受 MIT 协议影响。**
+---
+## 使用方式
+
+### 直接引入
+```html
+<script src="https://mirror.aigio1064.top/jquery/jquery.js"></script>
+```
+---
+### 间接引入
+
+```html
+<script a-srclist src="https://mirror.aigio1064.top/autoinduct.js"></script>
+```
+> 使用此方法需要使用 `a-srclist` 属性指定要引入的脚本
++  格式  
+```html
+<script a-srclist="src"></script>
+src改成需要的资源，写法：
+jqueryvuereact
+jquery;vue;react;
+jquery;vue;react
+jquery,vue,react
+格式随意只要保证单词拼写无误即可,按你自己的喜好来
+忽略大小写
+忽略先后顺序
+忽略分隔符
+```
+---
+## 可使用的资源  
+> 只保留最新发行资源，不保留历史版本，有需要则在正常 CDN 站点查看  
+
+| 资源标签 | 版本 | 资源名 |
+| :----: | :----: | :----: |
+| jquery | 3.6.1 | jquery.min.js |
+| vue | 2.7.13 | vue.min.js |
+| react | 16.8.0 | react.production.min.js |
---
+++ autoinduct.js
@@ -0,0 +1,47 @@
+/**
+ * Copyright 2022 Aigio1064
+ * MIT license
+*/
+(function(){
+    "use strict";
+    // 资源列表
+    let $resourcesList = {
+        jquery: [
+            "https://mirror.aigio1064.top/jquery/jquery.min.js", // 来自本储存库
+            "https://cdn.jsdelivr.net/npm/jquery@3.6.1/dist/jquery.min.js", // 来自https://www.jsdelivr.com/
+            "https://unpkg.com/jquery@3.6.1/dist/jquery.min.js" // 来自https://unpkg.com/
+        ],
+        vue: [
+            "https://mirror.aigio1064.top/vue/vue.min.js", // 来自本储存库
+            "https://cdn.jsdelivr.net/npm/vue@2.7.13/dist/vue.min.js", // 来自https://www.jsdelivr.com/
+            "https://unpkg.com/vue@2.7.13/dist/vue.min.js" // 来自https://unpkg.com/
+        ],
+        react: [
+            "https://mirror.aigio1064.top/react/react.production.min.js", // 来自本储存库
+            "https://cdn.jsdelivr.net/npm/react@18.2.0/cjs/react.production.min.js", // 来自https://www.jsdelivr.com/
+            "https://unpkg.com/react@18.2.0/cjs/react.production.min.js" // 来自https://unpkg.com/
+        ]
+    };
+    // 元素创建器 ↓ 这条代码遵循 Apache 协议，原作者还是我，源在另一个库里
+    function createNewElement(Parent,Element,Content,Attributes,Styles){let $Element=document.createElement(Element);if(!!Content){$Element.appendChild(document.createTextNode(Content));};if(!!Attributes){Attributes.forEach($a=>{let $b=$a.split("=");$Element.setAttribute($b[0],$b[1]);});};if(!!Styles){Styles.forEach($a=>{let $b=$a.split("=");$Element.style[$b[0]]=$b[1];});};document.querySelector(Parent).appendChild($Element);};
+    // 检测需要的资源
+    let $el = document.querySelector("[a-srclist]").getAttribute("a-srclist");
+    if (RegExp("jquery","i").test($el)) {
+        $resourcesList.jquery.forEach($a => {
+            createNewElement("head","script","",["src="+$a])
+        })
+    };
+    if (RegExp("vue","i").test($el)) {
+        $resourcesList.vue.forEach($a => {
+            createNewElement("head","script","",["src="+$a])
+        })
+    };
+    if (RegExp("react","i").test($el)) {
+        $resourcesList.react.forEach($a => {
+            createNewElement("head","script","",["src="+$a])
+        })
+    };
+    // 释放变量
+    $resourcesList = undefined;
+    $el = undefined;
+})()
